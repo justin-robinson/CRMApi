578f66784cd284e4463288665d28f01169646ccd
Justin Robinson <jurobn@amazon.com>
2018-05-09 04:53:19 -0400

    [master] cleanup client instantiation

--- CRMApi/AWS/DynamoDB/Client.cs
+++ CRMApi/AWS/DynamoDB/Client.cs
@@ -12,19 +12,18 @@ namespace CRMApi.AWS.DynamoDB {
 		public static void CreateClient(string profileName, string regionEndpointName) {
 			var chain = new CredentialProfileStoreChain ();
             AWSCredentials crmApiCredentials;
-			if (chain.TryGetAWSCredentials (profileName, out crmApiCredentials)) {
+			if (chain.TryGetAWSCredentials(profileName, out crmApiCredentials)) {
 				client = new AmazonDynamoDBClient(crmApiCredentials, RegionEndpoint.GetBySystemName (regionEndpointName));
             }
         }
 
 		public static void CreateClient(string serviceURL) {
-            AmazonDynamoDBConfig ddbConfig = new AmazonDynamoDBConfig {
-                ServiceURL = serviceURL
-            };
             try {
-                client = new AmazonDynamoDBClient (ddbConfig);
+                client = new AmazonDynamoDBClient(new AmazonDynamoDBConfig {
+                    ServiceURL = serviceURL
+                });
             } catch (Exception ex) {
-                Console.WriteLine ("\n Error: failed to create a DynamoDB client; " + ex.Message);
+                Console.WriteLine("\n Error: failed to create a DynamoDB client; " + ex.Message);
             }
 		}
     }

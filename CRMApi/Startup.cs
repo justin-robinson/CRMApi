0ef6d67c066d6da161c231168911f8f76bbba6d0
Justin Robinson <jurobn@amazon.com>
2018-06-04 01:47:41 -0400

    [dev] cross origin request for dev mode

--- CRMApi/Startup.cs
+++ CRMApi/Startup.cs
@@ -20,12 +20,14 @@ namespace CRMApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
             services.AddMvc();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
+                app.UseCors(builder => builder.WithOrigins("*"));
             }
 
 

a5ccd6244873d73fc69b6ce93af9f018bb04afe8
Justin Robinson <jurobn@amazon.com>
2018-06-06 17:00:13 -0400

    [master] post hero image url

--- CRMApi/Models/Post.cs
+++ CRMApi/Models/Post.cs
@@ -8,17 +8,19 @@ namespace CRMApi.Models {
         [DynamoDBProperty] public Guid AuthorId { get; set; }
         [DynamoDBProperty] public string Content { get; set; }
         [DynamoDBProperty] public string Title { get; set; }
+        [DynamoDBProperty] public Uri HeroImageUri{ get; set; }
         [DynamoDBProperty] public DateTime CreateTime { get; set; }
         [DynamoDBProperty] public DateTime UpdateTime { get; set; }
         [DynamoDBProperty] public DateTime DeleteTime { get; set; }
 
         public Post() { }
 
-        public Post(Guid postId, Guid authorId, string content, string title, DateTime createTime, DateTime updateTime, DateTime deleteTime) {
+        public Post(Guid postId, Guid authorId, string content, string title, Uri heroImageUri, DateTime createTime, DateTime updateTime, DateTime deleteTime) {
             PostId = postId;
             AuthorId = authorId;
             Content = content;
             Title = title;
+            HeroImageUri = heroImageUri;
             CreateTime = createTime;
             UpdateTime = updateTime;
             DeleteTime = deleteTime;

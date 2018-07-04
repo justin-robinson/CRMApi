5938e136f55beae2c7083ef1b9126bbdbf113001
Justin Robinson <jurobn@amazon.com>
2018-07-04 14:17:00 -0400

    [master] add table definitions

---
+++ CRMApi/TableDefinitions/Posts.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using Amazon.DynamoDBv2.Model;
+
+namespace CRMApi.TableDefinitions {
+    public class Post {
+        private const string TABLE = "Posts";
+        private const string HASH_KEY = "PostId";
+        public static CreateTableRequest TableRequest() {
+            return new CreateTableRequest {
+                TableName = TABLE,
+                AttributeDefinitions = new List<AttributeDefinition>{
+                    new AttributeDefinition {
+                        AttributeName = HASH_KEY,
+                        AttributeType = "S"
+                    },
+                    new AttributeDefinition {
+                        AttributeName = "AuthorId",
+                        AttributeType = "S"
+                    },
+                    new AttributeDefinition {
+                        AttributeName = "Content",
+                        AttributeType = "S"
+                    },
+                    new AttributeDefinition {
+                        AttributeName = "Title",
+                        AttributeType = "S"
+                    },
+                    new AttributeDefinition {
+                        AttributeName = "CreateTime",
+                        AttributeType = "S"
+                    },
+                    new AttributeDefinition {
+                        AttributeName = "UpdateTime",
+                        AttributeType = "S"
+                    },
+                    new AttributeDefinition {
+                        AttributeName = "DeleteTime",
+                        AttributeType = "S"
+                    }
+                },
+                KeySchema = new List<KeySchemaElement> {
+                    new KeySchemaElement {
+                        AttributeName = HASH_KEY,
+                        KeyType = "HASH"
+                    }
+                },
+                ProvisionedThroughput = new ProvisionedThroughput {
+                    ReadCapacityUnits = 5,
+                    WriteCapacityUnits = 5
+                }
+            };
+        }
+    }
+}
